9586a387925fbd17b841dc4b245f0c47845a0997
Iman Iqor <imiqor@c4r9p4.1337.ma>
2024-11-28 00:16:28 +0100

    hadchi

--- get_next_line.c
+++ get_next_line.c
@@ -1,26 +1,52 @@
-#include"get_next_line.h"
+#include "get_next_line.h"
 
-char* read_line(char* buff,char* line)
+char *ft_extract_line(char *str)
 {
-    
+    int i = 0;
+    int j;
+    char *line;
+
+    while (str[i] && str[i] != '\n')
+        i++;
+
+    line = malloc(i + 2);
+    if (!line)
+        return NULL;
+
+    j = 0;
+    while(j < i)
+    {
+        line[j] = str[j];
+    }
+    if (str[i] == '\n')
+        line[i++] = '\n';
+    line[i] = '\0';
+
+    return line;
 }
 
 char *get_next_line(int fd)
 {
-    static char line[BUFFER_SIZE+1];
-    ssize_t i;
-    int j;
-    char *buff;
-    buff = malloc(BUFFER_SIZE+1);
-    if(!buff)
-        return NULL;
-    j = 0;
-    i = read(fd,buff,BUFFER_SIZE+1); 
-f    if(i == -1)
-        return NULL;
-    
-    while(buff[j] && buff[j] != '\n')
+    static char *save;
+    char buff[BUFFER_SIZE + 1];
+    char *line;
+
+    int readed;
+    while ((readed = read(fd, buff, BUFFER_SIZE)) > 0)
     {
-        read_line(buff,line);
+        buff[readed] = '\0';
+        save = ft_strjoin(save, buff);
     }
+    if (!save)
+        return NULL;
+    line = ft_extract_line(save);
+    return line;
+}
+
+int main()
+{
+    int fd1 = open("txt1.txt", O_RDONLY);
+    char *s;
+    s = get_next_line(fd1);
+    printf("%s", s);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,17 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
-
-#include<unistd.h>
-# include <fcntl.h>
-#include<stdlib.h>
-
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif 
+#define BUFFER_SIZE 10
+#endif
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *ft_strjoin(char *save, char *buff);
+char *ft_strdup(char *s);
+int ft_strlen(char *s);
+char *ft_strchr(char* str, int c);
 #endif
